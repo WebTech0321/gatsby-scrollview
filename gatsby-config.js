486a73e4ac8fb8787083320e5a30340f84a00797
Andy <volleyvolley321@hotmail.com>
2021-06-04 20:53:22 +0100

    -scroll view

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,6 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-sass",
-    "gatsby-plugin-gatsby-cloud",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
     "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-manifest",
